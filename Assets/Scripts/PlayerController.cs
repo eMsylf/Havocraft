9e955ea78400eba14b69dab7da554cd24a7466fb
eMsylf <bobjeltes@hotmail.com>
2020-07-29 19:47:38 +0200

    Additions - Made more things for broken hovercrafts - Added an animation for moving hovercrafts
    Changes
    - Made the player hovercrafts more hovercrafty
    
    Issues
    - Movement animation also causes the rigidbody's axes to shift so that's an issue

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
+using UnityEngine.VFX;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
@@ -34,20 +35,22 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public Transform Body;
+
     public float Speed = 3f;
     public float RotationSpeed = 1f;
 
     private void Awake()
     {
-        Controls.InGame.Movement.performed += _ => Move(_.ReadValue<Vector2>());
-        Controls.InGame.Movement.canceled += _ => StopMove();
+        //Controls.InGame.Movement.performed += _ => Move(_.ReadValue<Vector2>());
+        //Controls.InGame.Movement.canceled += _ => StopMove();
         Controls.InGame.Shoot.performed += _ => Shoot();
         Controls.InGame.Boost.performed += _ => Boost(true);
         Controls.InGame.Boost.canceled += _ => Boost(false);
     }
 
     bool boostActivated = false;
-    float boostSpeedMultiplier = 2f;
+    public float boostSpeedMultiplier = 2f;
     private void Boost(bool enabled)
     {
         boostActivated = enabled;
@@ -57,14 +60,27 @@ public class PlayerController : MonoBehaviour
     {
     }
 
+    public bool Animation = true;
+    [Range(0f, 90f)]
+    public float AnimationTiltAngle = 10f;
+    [Range(0f, 1f)]
+    public float AnimationRigidity = .5f;
+
     void Update()
     {
+        movementInput = controls.InGame.Movement.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
         Rigidbody.AddRelativeForce(0f, 0f, movementInput.y * Speed * (boostActivated?boostSpeedMultiplier:1f));
         Rigidbody.AddTorque(0f, movementInput.x * RotationSpeed, 0f);
+        if (Animation)
+        {
+            Vector3 rotation = Body.localRotation.eulerAngles;
+            rotation.x = movementInput.y * AnimationTiltAngle;
+            Body.localRotation = Quaternion.Lerp(Body.localRotation, Quaternion.Euler(rotation), AnimationRigidity);
+        }
     }
 
     Vector2 movementInput;
@@ -108,22 +124,40 @@ public class PlayerController : MonoBehaviour
         if (projectile != null)
         {
             ExplodeViolently();
+            TurnOffHoverjets();
         }
     }
 
-    public GameObject deathObject;
+    public List<GameObject> HoverJets;
+    public List<GameObject> DeathEffectObjects;
 
     private void ExplodeViolently()
     {
-        //foreach (MeshRenderer renderer in GetComponentsInChildren<MeshRenderer>())
-        //{
-        //    renderer.enabled = false;
-        //}
-        enabled = false;
         Rigidbody.constraints = RigidbodyConstraints.None;
-        if (deathObject != null)
+        Rigidbody.useGravity = true;
+        if (DeathEffectObjects != null)
         {
-            Instantiate(deathObject, transform, true);
+            foreach (GameObject obj in DeathEffectObjects)
+            {
+                obj.SetActive(true);
+                VisualEffect vfx = obj.GetComponent<VisualEffect>();
+                // Give the visual effect additional velocity
+                if (vfx != null)
+                {
+                    if (vfx.HasVector3("Additional Velocity"))
+                        vfx.SetVector3("Additional Velocity", Rigidbody.velocity);
+                }
+            }
+            
+        }
+        enabled = false;
+    }
+
+    void TurnOffHoverjets()
+    {
+        foreach (GameObject hoverJet in HoverJets)
+        {
+            hoverJet.SetActive(false);
         }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,10 +8,15 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public PlayerController Owner;
+    public float ExplosionForce = 10f;
+    public float ExplosionRadius = 2f;
+    public float UpwardsModifier = 1f;
 
     Vector3 start;
     Vector3 impact;
     bool hasImpacted = false;
+
+    public List<Rigidbody> ExplodedRbList = new List<Rigidbody>();
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -25,8 +30,19 @@ public class Projectile : MonoBehaviour
 
         impact = transform.position;
         hasImpacted = true;
+        foreach (Collider col in Physics.OverlapSphere(impact, ExplosionRadius))
+        {
+            if (col.attachedRigidbody != null)
+            {
+                col.attachedRigidbody.AddExplosionForce(ExplosionForce, impact, ExplosionRadius, UpwardsModifier);
+                col.attachedRigidbody.AddTorque(ExplosionForce, 0f, 0f);
+                ExplodedRbList.Add(col.attachedRigidbody);
+                Debug.Log("Add explosion force to " + col.name, col);
+            }
+        }
+        //collision.rigidbody?.AddExplosionForce(ExplosionForce, impact, ExplosionRadius, UpwardsModifier);
 
-        // Disable the object
+        // Disable the projectile
         GetComponent<Collider>().enabled = false;
         GetComponent<Rigidbody>().isKinematic = true;
         GetComponent<MeshRenderer>().enabled = false;
@@ -43,5 +59,10 @@ public class Projectile : MonoBehaviour
         if (!hasImpacted)
             Gizmos.DrawRay(transform.position, GetComponent<Rigidbody>().velocity);
         Gizmos.DrawLine(start, transform.position);
+
+        foreach (Rigidbody rb in ExplodedRbList)
+        {
+            Gizmos.DrawLine(transform.position, rb.position);
+        }
     }
 }
