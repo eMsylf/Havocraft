3cc1735acbc6b06a133c6d221c2c9c4109a2cf18
eMsylf <bobjeltes@hotmail.com>
2020-08-05 17:13:56 +0200

    - Started working on menus - Imported Text Mesh Pro

---
+++ Assets/Menus/Scripts/MenuManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class MenuManager : MonoBehaviour
+{
+    public Transform Screens;
+
+    public int GetScreenCount()
+    {
+        return transform.childCount;
+    }
+
+    public void NextScreen()
+    {
+        Debug.Log("Next Screen");
+        if (Screens == null)
+        {
+            return;
+        }
+
+        Screens.Translate(-Screen.width, 0f, 0f);
+    }
+
+    public void PreviousScreen()
+    {
+        Debug.Log("Previous Screen");
+        if (Screens == null)
+        {
+            return;
+        }
+        Screens.Translate(Screen.width, 0f, 0f);
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
