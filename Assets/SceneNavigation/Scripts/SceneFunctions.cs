93ab799be23dba5cc8ebc088c346f643b4553f52
Bob <bobjeltes@hotmail.com>
2021-01-21 22:43:12 +0100

    Import scene functions, Login screen's "Register" button now loads register/edit page

--- Assets/SceneNavigation/Scripts/SceneFunctions.cs
+++ Assets/SceneNavigation/Scripts/SceneFunctions.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using BobJeltes.StandardUtilities;
 
-public static class SceneFunctions
+public class SceneFunctions : Singleton<SceneFunctions>
 {
-    public static void NextScene()
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
